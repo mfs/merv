1936b1a7149c0bed507326081ad715bab9b32be6
Mike Sampson <mike@sambodata.com>
2015-03-21 13:39:38 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+merv
+====
+
+## Description
+
+A simple utility for helping to solve crosswords. At the moment it is basically
+a simplified grep though I have some ideas for extra functionality.
+
+Merv expects to find a dictionary file at `/usr/share/dict/merv`. Create a
+symbolic link to `/usr/share/dict/words` if it exists or drop in a suitable word
+list. [Puzzlers' League][0] has links to suitable lists.
+
+## Usage
+
+Usage is simple. Pass the word you are looking for with blanks replaced with '.'
+
+    $ merv .x..p.e
+    example
+    exciple
+
+[0]: http://www.puzzlers.org/dokuwiki/doku.php?id=solving:wordlists:about:start 
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+// matches a pattern such as 'e..mp..'
+// against a string such as  'example'
+fn pmatch(pattern: &str, target: &str) -> bool {
+    // early out
+    if pattern.len() != target.len() {
+        return false;
+    }
+    for (x, y) in pattern.chars().zip(target.chars()) {
+        if x == y || x == '.' {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: {} <PATTERN>", args[0]);
+        return;
+    }
+
+    let dict_file = "/usr/share/dict/merv";
+
+    let file = match File::open(dict_file) {
+        Ok(file) => file,
+        Err(err) => { println!("Error opening {}. {}", dict_file, err); return; }
+    };
+
+    let buf = BufReader::new(&file);
+
+    let t = args[1].clone();
+
+    for line in buf.lines() {
+        let s = line.unwrap();
+
+        if pmatch(&t, &s) {
+            println!("{}", s);
+        }
+    }
+}
